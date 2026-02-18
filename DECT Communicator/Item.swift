46d7b901da70a352e328c9cef9feb5fe35a45b3d
Evan Stoddard <evan.stoddard@me.com>
2026-02-18 10:21:58 -0600

    Initial Commit

---
+++ DECT Communicator/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  DECT Communicator
+//
+//  Created by Evan Robert Stoddard on 2/18/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
